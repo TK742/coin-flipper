c3a6af9e3a8871544468e4e7b34e50c0d324f801
Tom <ttasca742@gmail.com>
2021-11-24 22:02:39 -0300

    sfx added

--- Assets/DEVELOPMENT/ILogic.cs
+++ Assets/DEVELOPMENT/ILogic.cs
@@ -6,7 +6,10 @@ public class ILogic : MonoBehaviour
 {
     [SerializeField] private GameObject ui;
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private AudioClip win, lose, drumRoll, wtf;
+    [SerializeField] private AudioSource sfx, music;
     private Camera main;
+    private float timer;
     public void Flip() => StartCoroutine(IFlip());
     private bool Moving => rb.velocity.magnitude > 0f;
 
@@ -18,17 +21,44 @@ public class ILogic : MonoBehaviour
 
     private void Update()
     {
+        if (!Moving) timer += Time.deltaTime;
         main.transform.LookAt(rb.transform);
         main.fieldOfView = Mathf.Lerp(main.fieldOfView, Moving? 15f : 30f, Time.deltaTime * 10f);
     }
 
     private IEnumerator IFlip()
     {
+        timer = 0;
+        music.Stop();
+        Play(drumRoll);
         ui.SetActive(false);
-        rb.AddForce(Vector3.up * Random.Range(250f, 500f));
+        rb.AddForce(Vector3.up * Random.Range(1000, 2500));
         yield return new WaitForSeconds(Random.Range(0.10f, 0.30f));
-        rb.AddTorque(Vector3.forward * Random.Range(100f, 1000f));
-        yield return new WaitUntil(() => !Moving);
+        rb.AddTorque(Vector3.forward * Random.Range(2500, 5000));
+        yield return new WaitUntil(() => !Moving && timer >= 0.5f);
+        sfx.Stop();
+        yield return new WaitUntil(() => !Moving && timer >= 1f);
+
+        switch (rb.rotation.eulerAngles.x)
+        {
+            case 90:
+                Play(win);
+                break;
+            case 270:
+                Play(lose);
+                break;
+            default:
+                Play(wtf);
+                break;
+        }
+
+        music.Play();
         ui.SetActive(true);
+    }
+
+    private void Play(AudioClip clip)
+    {
+        sfx.clip = clip;
+        sfx.Play();
     }
 }
